c5cb554cc28822c59b62b1fb88a286caf2e1beb4
IvNo <73193154@fsv.cuni.cz>
2023-07-16 10:53:28 +0200

    change of graphs

--- abm_app.py
+++ abm_app.py
@@ -5,11 +5,14 @@ from mesa.datacollection import DataCollector
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Citizen(Agent): 
-    def __init__(self, unique_id, model, wealth):
+    def __init__(self, unique_id, model, wealth,x,y):
         super().__init__(unique_id, model)
         self.wealth = wealth  
+        self.x = x
+        self.y = y
 
     def step(self):
         self.wealth -= self.model.fuel_price * self.model.fuel_units
@@ -27,11 +30,13 @@ class World(Model):
 
         poor_threshold = self.num_agents * poor_percentage
         for i in range(self.num_agents):
+            x = i%100
+            y = i // 100
             if i < poor_threshold:  
-                a = Citizen(i, self, 100)  
+                a = Citizen(i, self, 100, x, y)  
             else:
-                a = Citizen(i, self, 10000)  
-            self.schedule.add(a)  
+                a = Citizen(i, self, 10000, x, y)  
+            self.schedule.add(a)
 
     def step(self):
         self.datacollector.collect(self)
@@ -43,7 +48,31 @@ class World(Model):
             if agent.wealth < self.fuel_price * self.fuel_units:  
                 count += 1
         return count / self.num_agents
+    
+def plot_heatmap(year):
+    agent_data = model.datacollector.get_agent_vars_dataframe()
+    data = agent_data.xs(year, level="Step")["Wealth"]
+    # Create a 100x100 array filled with zeros
+    heatmap_data = np.zeros((100, 100))
+    for i in range(model.num_agents):
+        # For each agent, set the corresponding cell in the array to their wealth
+        agent_wealth = data[i]
+        x = model.schedule.agents[i].x
+        y = model.schedule.agents[i].y
+        heatmap_data[y][x] = agent_wealth
+    # Normalize wealth values to range from 0 to 1 for better coloring
+    heatmap_data = (heatmap_data - np.min(heatmap_data)) / (np.max(heatmap_data) - np.min(heatmap_data))
+    # Plot the heatmap
+    fig, ax = plt.subplots(figsize = (5,5))
+    sns.heatmap(heatmap_data, cmap='coolwarm', ax=ax)
+    st.pyplot(fig)
 
+
+##########################
+### STREAMLIT SETTINGS ###
+
+# Set the width of the layout
+st.set_page_config(layout="wide")
 st.title('energy poverty and technological progress ABM')
 
 # User inputs
@@ -69,10 +98,14 @@ year = st.slider('Year', min_value=0, max_value=10, step=1, value=0)
 agent_data = model.datacollector.get_agent_vars_dataframe()
 data = agent_data.xs(year, level="Step")["Wealth"]
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize = (5,5))
 plt.hist(data, bins=range(-1000, 11000, 500))
 plt.xlabel('Wealth')
 plt.ylabel('Number of Citizens')
 plt.title('Wealth Distribution in Year {}'.format(year))
 plt.grid(True)
 st.pyplot(fig)
+
+#Heatmap
+
+plot_heatmap(year)
