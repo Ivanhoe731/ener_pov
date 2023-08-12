d1aae00c90274a21f4f5ef95248ac0431e237022
IvNo <73193154@fsv.cuni.cz>
2023-08-12 04:08:34 +0200

    0k

--- info_page.py
+++ info_page.py
@@ -3,7 +3,7 @@ import streamlit as st
 # Page: Info
 def info_page():
     st.title("Info Page")
-    st.header("About the Model")
+    st.header("About the Model - I am currently experiencing technical issues uploading more detailed description of the model, as well as a better version of the model itself, update is planned for 13.08.2023")
     st.write("The model proposed on this page is an exploratory model, trying to explore how energy poverty under technological progress could be simulated. Agents are households with some disposable income and some characteristics, most notably 'inability', which dentoes wheather the households have an inability to keep home adequately warm. The agents are subject to prices of two fuels - yellow fuel and brown fuel and if activated, then some govenrmental policies / programs. ")
 
     st.header("How It Works")
